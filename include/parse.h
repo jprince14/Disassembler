284e86e936fa26536f4412eaa54e9d25ea3d3bae
Jeremy <jeremy@localhost.localdomain>
2015-06-13 15:28:03 -0400

    New files

---
+++ include/parse.h
@@ -0,0 +1,8 @@
+#ifndef PARSE_H_
+#define PARSE_H_
+
+
+
+#endif /* PARSE_H_ */
+
+
--- src/Disassemler.c
+++ src/Disassemler.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/Disassembler.h"
+#include "../include/parse.h"
 
 
 size_t readopcode(FILE* IN_fp, FILE* OUT_fp, u8* buffer) {
@@ -11,31 +13,9 @@ size_t readopcode(FILE* IN_fp, FILE* OUT_fp, u8* buffer) {
 		printf("Error reading file\n");
 		exit(-2);
 	}
+	parseopcode(IN_fp, OUT_fp, buffer);
 
 	return size;
-}
-
-//a short int is 2 bytes and code is only 1  byte
-void parseopcode(FILE* IN_fp, FILE* OUT_fp, unsigned char* buffer) {
-	size_t size;
-
-	switch (buffer[0]) {
-	case 0x00:
-		//do something
-		size = fread(buffer, 1, 1, IN_fp);
-		if (size != 1) {
-			fprintf(stderr, "Either End of File or Error Reading\n");
-			exit(-1);
-		}
-		char str[] = "Testing";
-		fwrite(str,1, sizeof(str), OUT_fp);
-		break;
-
-	default:
-		fprintf(stderr, "ERROR: Opcode of 0x%x not recognized\n", buffer[0]);
-
-	}
-
 }
 
 int main(int argc, char *argv[]) {
